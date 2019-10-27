e630f6453b737203079796ea02ee75f42564ea39
박정현 <pjh9996@naver.com>
2019-10-27 19:05:57 +0900

    Blockly 데모 추가

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainActivity.java
@@ -3,12 +3,23 @@ package kmucs.mobileprogramming.team.a.blocklylmc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    WebSettings webSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/blockly/webview.html");
     }
 }
