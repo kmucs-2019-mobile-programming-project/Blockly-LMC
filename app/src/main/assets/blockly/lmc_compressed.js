347b0bff2f9dfb1f3ea43e2ff077989900a35d13
박정현 <pjh9996@naver.com>
2019-11-05 22:36:12 +0900

    Update lmc_compressed.js

--- app/src/main/assets/blockly/lmc_compressed.js
+++ app/src/main/assets/blockly/lmc_compressed.js
@@ -2,16 +2,16 @@
 
 Blockly.LMC = new Blockly.Generator("LMC");
 
-Blockly.Blocks['move_right'] = {
+Blockly.Blocks['lda'] = {
   init: function() {
-    this.appendValueInput("PIXELS")
-        .setCheck("Number")
-        .appendField("move to right");
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(120);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+    this.appendDummyInput()
+        .appendField("LDA")
+        .appendField(new Blockly.FieldNumber(0, 0, 99), "ADDR");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
 };
 
@@ -20,4 +20,13 @@ Blockly.LMC['lda']=function(block)
     var addr = block.getFieldValue('ADDR');
     var code="1"+addr;
     return code;
+}
+
+function generateCode()
+{
+    try{
+        window.AndroidAPP.getCode(Blockly.LMC.workspaceToCode(workspacePlayground));
+    }catch(err){
+        console.log("[generateCode()]: " + err);
+    }
 }
