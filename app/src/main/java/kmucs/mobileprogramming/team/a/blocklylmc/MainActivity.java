42664baaaf5d4894dd8618be50ab1fcedbd0dbab
박정현 <pjh9996@naver.com>
2019-11-05 22:36:14 +0900

    Update MainActivity.java

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainActivity.java
@@ -2,14 +2,26 @@ package kmucs.mobileprogramming.team.a.blocklylmc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private final Handler handler = new Handler();
+
     WebView webView;
     WebSettings webSettings;
+
+    Button btn_run;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +33,19 @@ public class MainActivity extends AppCompatActivity {
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/blockly/webview.html");
+        webView.addJavascriptInterface(this, "AndroidAPP");
+
+        btn_run = findViewById(R.id.btn_run);
+        btn_run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("javascript:generateCode()");
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getCode(String code){
+        Toast.makeText(getApplicationContext(), code, Toast.LENGTH_LONG).show();
     }
 }
