3625b0db75f13828ea5d01c3eb967025b40f3b56
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-11-12 15:06:16 +0900

    Update lmc_compressed.js
    LMC 각 명령어에 대한 code generator 전부 구현 완료

--- app/src/main/assets/blockly/lmc_compressed.js
+++ app/src/main/assets/blockly/lmc_compressed.js
@@ -10,16 +10,92 @@ Blockly.Blocks['lda'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
+
+Blockly.LMC['add']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "1" + zeropad(addr, 2);
+    return code;
+}
+
+Blockly.LMC['sub']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "2" + zeropad(addr, 2);
+    return code;
+}
+
+Blockly.LMC['sta']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "3" + zeropad(addr, 2);
+    return code;
+}
 
 Blockly.LMC['lda']=function(block)
 {
     var addr = block.getFieldValue('ADDR');
-    var code="1"+addr;
+    var code = "4" + zeropad(addr, 2);
     return code;
+}
+
+Blockly.LMC['jmp']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "5" + zeropad(addr, 2);
+    return code;
+}
+
+Blockly.LMC['jez']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "6" + zeropad(addr, 2);
+    return code;
+}
+
+Blockly.LMC['jnz']=function(block)
+{
+    var addr = block.getFieldValue('ADDR');
+    var code = "7" + zeropad(addr, 2);
+    return code;
+}
+
+Blockly.LMC['inp']=function(block)
+{
+    var code = "801";
+    return code;
+}
+
+Blockly.LMC['out']=function(block)
+{
+    var code = "802";
+    return code;
+}
+
+Blockly.LMC['cob']=function(block)
+{
+    var code = "900";
+    return code;
+}
+
+Blockly.LMC['dat']=function(block)
+{
+    var addr = block.getFieldValue('DATA');
+    var code = zeropad(addr, 3);
+    return code;
+}
+
+function zeropad(num, digit)
+{
+    num = num + '';
+    while(num.length < digit){
+        num = '0' + num;
+    }
+    return num;
 }
 
 function generateCode()
