452cb83ff02f0f3e47c98449dc07776622450255
park jongheum <whdgmawkd@naver.com>
2019-11-12 17:44:46 +0900

    lmc 에뮬레이터 메모리 페이져 추가
    스피너에서 10줄 단위로 선택

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/EmulatorActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/EmulatorActivity.java
@@ -1,13 +1,19 @@
 package kmucs.mobileprogramming.team.a.blocklylmc;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class EmulatorActivity extends AppCompatActivity {
 
     private GridLayout lmcRegisterView;
+    private Spinner lmcMemorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,14 @@ public class EmulatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_emulator);
 
         lmcRegisterView = findViewById(R.id.lmc_register_view);
+        lmcMemorySpinner = findViewById(R.id.lmc_memory_spinner);
+
+        // TODO: 11/12/2019 스피너 레이아웃 수정 
+//        ArrayList<String> lmcSpinnerItem = new ArrayList<>();
+//        for(int i=0;i<99;i+=10){
+//            lmcSpinnerItem.add(Integer.toString(i));
+//        }
+//        ArrayAdapter<String> lmcMemSpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, lmcSpinnerItem);
+//        lmcMemorySpinner.setAdapter(lmcMemSpinnerAdapter);
     }
 }
