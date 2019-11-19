07200d1d185296d89a38c762d555f55b53f0e634
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-11-19 20:51:08 +0900

    Update MainMenuActivity.java

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainMenuActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/MainMenuActivity.java
@@ -1,10 +1,14 @@
 package kmucs.mobileprogramming.team.a.blocklylmc;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -19,8 +23,32 @@ public class MainMenuActivity extends AppCompatActivity {
         btnChallenge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, ProblemListActivity.class);
-                startActivity(intent);
+                final EditText editText = new EditText(MainMenuActivity.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+                builder.setTitle("도전모드 입장")
+                        .setMessage("사용할 닉네임을 입력하세요")
+                        .setView(editText)
+                        .setCancelable(true)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String username = editText.getText().toString();
+                                if(username.length() > 0) {
+                                    SharedPreferences sharedPreferences = getSharedPreferences("User_Info", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("username", username);
+                                    editor.apply();
+                                    Intent intent = new Intent(MainMenuActivity.this, ProblemListActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        });
+                builder.show();
             }
         });
 
