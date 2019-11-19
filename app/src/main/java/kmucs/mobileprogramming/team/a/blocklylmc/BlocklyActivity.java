885264f6a75e31945fa427a4363e2f6e63ba6840
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-11-19 20:26:53 +0900

    Update BlocklyActivity.java

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/BlocklyActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/BlocklyActivity.java
@@ -44,7 +44,7 @@ public class BlocklyActivity extends AppCompatActivity {
                     public void onClick(View view) {
                         // TODO: Problem Viewer Activity에 lv값 넣어서 호출하기
                         Toast.makeText(getApplicationContext(), String.valueOf(lv), Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(BlocklyActivity.this, TutorialActivity.class);
+                        Intent intent = new Intent(BlocklyActivity.this, ProblemViewerActivity.class);
                         startActivity(intent);
                     }
                 });
