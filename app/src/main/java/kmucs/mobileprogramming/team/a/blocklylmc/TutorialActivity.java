e9f6630bfb9c9150dfccfde511333114a443d330
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-11-19 20:15:19 +0900

    Create TutorialActivity.java

---
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/TutorialActivity.java
@@ -0,0 +1,21 @@
+package kmucs.mobileprogramming.team.a.blocklylmc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TutorialActivity extends Activity {
+    int[] layout = {R.layout.activity_tutorial_problemlist, R.layout.activity_tutorial_blockly};
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            int layout_idx = bundle.getInt("layout_idx", -1);
+            if(layout_idx == -1) finish();
+            setContentView(layout[layout_idx]);
+        }
+        else finish();
+    }
+}
