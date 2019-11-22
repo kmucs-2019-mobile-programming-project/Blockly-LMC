3c697ee42132629af63e1493644f884759772646
parkjongheum <parkjh0821@gmail.com>
2019-11-23 00:48:11 +0900

    EmulatorActivity 싸이클 표시 추가

--- app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/EmulatorActivity.java
+++ app/src/main/java/kmucs/mobileprogramming/team/a/blocklylmc/EmulatorActivity.java
@@ -35,7 +35,7 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
 
     private ImageButton buttonStep, buttonReset;
 
-    private TextView lmcPC, lmcACC, lmcIR, lmcPSR;
+    private TextView lmcPC, lmcACC, lmcIR, lmcPSR, lmcCycle;
 
     private int numberPickerValue;
 
@@ -73,6 +73,7 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
         lmcACC = findViewById(R.id.lmc_acc);
         lmcIR = findViewById(R.id.lmc_ir);
         lmcPSR = findViewById(R.id.lmc_psr);
+        lmcCycle = findViewById(R.id.lmc_cycle);
         // TODO: 11/12/2019 스피너 레이아웃 수정 
 //        ArrayList<String> lmcSpinnerItem = new ArrayList<>();
 //        for(int i=0;i<99;i+=10){
@@ -81,25 +82,25 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
 //        ArrayAdapter<String> lmcMemSpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, lmcSpinnerItem);
 //        lmcMemorySpinner.setAdapter(lmcMemSpinnerAdapter);
     }
-    
-    void refreshRecycler(int position){
-        ((MemoryRecyclerAdapter)memoryRecyclerView.getAdapter()).setLmcData(machine.getMailBoxes());
+
+    void refreshRecycler(int position) {
+        ((MemoryRecyclerAdapter) memoryRecyclerView.getAdapter()).setLmcData(machine.getMailBoxes());
         memoryRecyclerView.getAdapter().notifyDataSetChanged();
         ((MemoryRecyclerAdapter) memoryRecyclerView.getAdapter()).setCurrentRow(position);
-        ((LinearLayoutManager) memoryRecyclerView.getLayoutManager()).scrollToPositionWithOffset(position,memoryRecyclerView.getChildAt(0).getHeight());
+        ((LinearLayoutManager) memoryRecyclerView.getLayoutManager()).scrollToPositionWithOffset(position, memoryRecyclerView.getChildAt(0).getHeight());
         // memoryRecyclerView.scrollToPosition(position);
 
     }
 
-    int lmcINP(){
+    int lmcINP() {
         // input dialog
         NumberPickerDialog numberPickerDialog = new NumberPickerDialog();
         numberPickerDialog.setValueChangeListener(this);
-        numberPickerDialog.show(this.getSupportFragmentManager(),"number picker");
+        numberPickerDialog.show(this.getSupportFragmentManager(), "number picker");
         return numberPickerValue;
     }
 
-    void lmcOUT(int value){
+    void lmcOUT(int value) {
         // output dialog
         AlertDialog.Builder outputDialog = new AlertDialog.Builder(this);
         outputDialog.setTitle("Output")
@@ -117,11 +118,11 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         int viewId = v.getId();
-        switch(viewId) {
+        switch (viewId) {
             case R.id.lmc_step:
                 try {
                     machine.step();
-                } catch (OutOfMailboxException e){
+                } catch (OutOfMailboxException e) {
                     // error dialog
                     onClick(findViewById(R.id.lmc_reset));
                     return;
@@ -131,12 +132,12 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
                     Toast.makeText(this, "wait inp", Toast.LENGTH_SHORT).show();
                     //machine.setIO(lmcINP());
                     lmcINP();
-                    Log.d("emu","io" + numberPickerValue);
+                    Log.d("emu", "io" + numberPickerValue);
                 } else if (machine.getWaitOUT()) {
                     // output dialog
                     Toast.makeText(this, "wait out", Toast.LENGTH_SHORT).show();
                     lmcOUT(machine.getIO());
-                    Log.d("emu","io" + machine.getA());
+                    Log.d("emu", "io" + machine.getA());
                 }
                 int current = machine.getPC();
                 refreshRecycler(current);
@@ -162,17 +163,18 @@ public class EmulatorActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if(id == android.R.id.home){
+        if (id == android.R.id.home) {
             finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    void updateRegister(){
-        lmcPC.setText(Integer.toString(machine.getPC()));
-        lmcACC.setText(Integer.toString(machine.getA()));
-        lmcIR.setText(Integer.toString(machine.getIR()));
-        lmcPSR.setText(Integer.toString(machine.getPSR()));
+    void updateRegister() {
+        lmcPC.setText(String.format("%03d", machine.getPC()));
+        lmcACC.setText(String.format("%03d", machine.getA()));
+        lmcIR.setText(String.format("%03d", machine.getIR()));
+        lmcPSR.setText(String.format("%03d", machine.getPSR()));
+        lmcCycle.setText(Integer.toString(machine.getCYCLE()));
     }
 }
